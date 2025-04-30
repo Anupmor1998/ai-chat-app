5b8d66a955d916e54ab5c765f4874e0461ea4da2
Anupmor1998 <anupmor1998@gmail.com>
2025-04-30 21:29:06 +0530

    initial project setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const authRouter = require("./routes/authRouter");
+const connectDB = require("./utils/db");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8000;
+
+app.use("/api/auth", authRouter);
+
+app.all("{*splat}", (req, res, next) => {
+  res.send(`the requested path: ${req.originalUrl} not found`);
+  next();
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/models/userModal.js
@@ -0,0 +1,32 @@
+const { Schema, mongo, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  profileImage: {
+    type: String,
+  },
+  chats: [String],
+});
+
+module.exports = mongoose.model("UserModal", userSchema);
